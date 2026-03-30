b21cbaa1e1a83f52f11020003af8bc81ace78c60
Christian Pflugradt <110765548+christianpflugradt@users.noreply.github.com>
2026-03-30 20:51:13 +0200

    Add renderer icon generation script and PWA/favicon assets

---
+++ renderer/scripts/generate-icons.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+set -eu
+
+SCRIPT_DIR="$(CDPATH= cd -- "$(dirname -- "$0")" && pwd)"
+PUBLIC_DIR="$SCRIPT_DIR/../public"
+
+cd "$PUBLIC_DIR"
+
+MASTER_APP_ICON="icon-512.png"
+MASTER_FAVICON="favicon.png"
+
+magick "$MASTER_APP_ICON" -filter Lanczos -resize 192x192 "icon-192.png"
+magick "$MASTER_APP_ICON" -filter Lanczos -resize 180x180 "apple-touch-icon.png"
+
+magick "$MASTER_FAVICON" -filter Lanczos -resize 48x48 "favicon-48.png"
+magick "$MASTER_FAVICON" -filter Lanczos -resize 32x32 "favicon-32.png"
+magick "$MASTER_FAVICON" -filter Lanczos -resize 16x16 "favicon-16.png"
+
+magick "favicon-16.png" "favicon-32.png" "favicon-48.png" "favicon.ico"
+
+rm -f "favicon-16.png" "favicon-32.png" "favicon-48.png"
